a32a22f9290fa8ea0dcb4b249bc7104a908d8978
Bryan Aument <bryaument@gmail.com>
2025-03-05 16:18:47 -0500

    added mobile tooltip for better UX nav UX for vital details

--- src/components/vital/vital.tsx
+++ src/components/vital/vital.tsx
@@ -1,4 +1,12 @@
 import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useState, useRef, useEffect } from "react";
+import { useIsMobile } from "@/hooks/is-mobile";
 
 interface VitalProps {
   icon?: React.JSX.Element;
@@ -8,12 +16,46 @@ interface VitalProps {
   className?: string;
 }
 function Vital({ icon, name, vital, children, className }: VitalProps) {
+  const [showToolTip, setShowToolTip] = useState(false);
+  const isMobile = useIsMobile();
+  const vitalRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        vitalRef.current &&
+        !vitalRef.current.contains(event.target as Node)
+      ) {
+        setShowToolTip(false); // Close the tooltip
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
   return (
-    <li title={name} className={cn(`flex flex-col items-center ${className}`)}>
-      {icon}
-      {vital}
-      {children}
-    </li>
+    <TooltipProvider>
+      <Tooltip open={showToolTip}>
+        <TooltipTrigger
+          asChild
+          onMouseEnter={() => !isMobile && setShowToolTip(true)}
+          onMouseLeave={() => !isMobile && setShowToolTip(false)}
+          onClick={() => setShowToolTip((prev) => !prev)}
+        >
+          <li
+            ref={vitalRef}
+            className={cn(`flex flex-col items-center ${className}`)}
+          >
+            {icon}
+            {vital}
+            {children}
+          </li>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
