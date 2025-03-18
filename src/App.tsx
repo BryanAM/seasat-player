4dad5168b302c4a8589b0bb3dfd22ce266c1a0eb
Bryan Aument <bryaument@gmail.com>
2025-03-17 23:14:22 -0400

    tinkering with dimensions and abstracting the draggable into a hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 import Navigation from "./components/navigation/navigation";
 import Video from "./components/video/video";
 import { useIsMobile } from "./hooks/is-mobile";
-import { useEffect, useRef, useState } from "react";
+import { useDraggable } from "./hooks/useDraggable";
 
 function App() {
   const isMobile = useIsMobile();
-  const videoRef = useRef<HTMLDivElement>(null);
-  const videoDimensions = useRef({ h: 220, w: 300 });
-  const draggingRef = useRef<boolean>(false);
-  const animationFrameId = useRef<number>(null);
-  const initialMousePosRef = useRef({ x: 0, y: 0 });
-  const initialWindowPosRef = useRef({ x: 0, y: 0 });
-
-  const [coordinates, setCoordinates] = useState({
-    x: window.innerWidth - videoDimensions.current.w,
-    y: window.innerHeight - videoDimensions.current.h,
+  const [videoDimension, setVideoDimensions] = useState({
+    height: 200,
+    width: 300,
   });
-
-  const clamp = (value: number, min: number, max: number) => {
-    return Math.min(Math.max(value, min), max);
-  };
-
-  const handleResize = () => {
-    const rX = window.innerWidth - videoDimensions.current.w;
-    const rY = window.innerHeight - videoDimensions.current.h;
-    setCoordinates({ x: rX, y: rY });
-  };
-
-  /**
-   * Calculate mouse movement
-   */
-  const handlePointerMove = (event: PointerEvent) => {
-    if (draggingRef.current) {
-      if (animationFrameId.current)
-        cancelAnimationFrame(animationFrameId.current);
-      animationFrameId.current = requestAnimationFrame(() => {
-        const deltaX = event.clientX - initialMousePosRef.current.x;
-        const deltaY = event.clientY - initialMousePosRef.current.y;
-
-        let newX = initialWindowPosRef.current.x + deltaX;
-        let newY = initialWindowPosRef.current.y + deltaY;
-
-        newX = clamp(newX, 0, window.innerWidth - videoDimensions.current.w);
-        newY = clamp(newY, 0, window.innerHeight - videoDimensions.current.h);
-
-        setCoordinates({ x: newX, y: newY });
-      });
-    }
-  };
-
-  const handlePointerUp = (event: PointerEvent) => {
-    if (videoRef.current) {
-      draggingRef.current = false;
-      // Release pointer capture
-      videoRef.current.releasePointerCapture(event.pointerId);
-      videoRef.current.removeEventListener("pointermove", handlePointerMove);
-      videoRef.current.removeEventListener("pointerup", handlePointerUp);
-      videoRef.current.removeEventListener("pointercancel", handlePointerUp);
-    }
-  };
-
-  /**
-   * Detect if our mousedown is on draggable area
-   */
-  const handlePointerDown = (event: PointerEvent) => {
-    if (
-      videoRef.current &&
-      event.target instanceof Node &&
-      videoRef.current.contains(event.target)
-    ) {
-      // handle drag
-      draggingRef.current = true;
-      initialMousePosRef.current = { x: event.clientX, y: event.clientY };
-      initialWindowPosRef.current = { ...coordinates };
-      // Capture the pointer to ensure all events are sent to this element
-      videoRef.current.setPointerCapture(event.pointerId);
-      videoRef.current.addEventListener("pointermove", handlePointerMove);
-      videoRef.current.addEventListener("pointerup", handlePointerUp);
-      videoRef.current.addEventListener("pointercancel", handlePointerUp);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("pointerdown", handlePointerDown);
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("pointerdown", handlePointerDown);
-      window.removeEventListener("resize", handleResize);
-    };
-  });
+  const { videoRef, coordinates, draggingRef } = useDraggable(
+    videoDimension.height,
+    videoDimension.width
+  );
 
   return (
     <section className="flex flex-col bg-accent min-h-dvh">
@@ -98,6 +22,7 @@ function App() {
 
       <Video
         ref={videoRef}
+        dimensions={videoDimension}
         coordinates={coordinates}
         dragging={draggingRef.current}
       />
--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -1,16 +1,16 @@
-function Video({ coordinates, ref, dragging }) {
+function Video({ coordinates, ref, dragging, dimensions }) {
   return (
     <div
       role="dialog"
       aria-label="mini-player"
       ref={ref}
-      className="h-[220px] w-[300px] bg-red-500 rounded-2xl shadow-sm hover:shadow-xl fixed"
+      className="h-[220px] w-[300px] bg-zinc-800 rounded-2xl shadow-sm hover:shadow-xl fixed"
       style={{
         position: "fixed",
         transform: `translate(${coordinates.x}px, ${coordinates.y}px)`,
         transition: `${!dragging && "transform 500ms cubic-bezier(0.5,0,0,1)"}`,
-        width: "300px",
-        height: "200px",
+        height: `${dimensions.height}px`,
+        width: `${dimensions.width}px`,
         cursor: "pointer",
         userSelect: "none",
         touchAction: "none",
@@ -23,10 +23,12 @@ function Video({ coordinates, ref, dragging }) {
     >
       <iframe
         className="rounded-t-2xl"
+        width={dimensions.width}
+        height="75%"
         src="https://scoutui.com:8888/embed_player?urlServer=wss://scoutui.com:8443&streamName=rtsp://hull36:pacific1@10.0.36.5:554/front-low?&mediaProviders=WebRTC&autoplay=true"
       />
 
-      <div className="p-2">Other things down here</div>
+      <div className="p-2 text-white">Draggable Area</div>
     </div>
   );
 }
